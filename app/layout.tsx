5bb27ce82f3ff565d67b0d864e7fc24e40fd625b
YasMcs <yasbems@gmail.com>
2026-06-17 10:02:00 -0600

    revertir vistas extra y barra de navegacion

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,14 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <nav className="bg-gray-800 p-4 text-white flex gap-4">
-          <a href="/" className="hover:underline">Productos</a>
-          <a href="/categories" className="hover:underline">Categorías</a>
-          <a href="/orders" className="hover:underline">Mis Órdenes</a>
-        </nav>
-        {children}
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
